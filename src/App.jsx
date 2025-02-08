b06ea61356e6e3371966d42538d72c1f83c8b14e
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-02-08 23:55:17 +0530

    Remove commented-out Projects and Clients components from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
 import Contact from './sections/Contact.jsx';
-import Clients from './sections/Clients.jsx';
-import Projects from './sections/Projects.jsx';
+
 import WorkExperience from './sections/Experience.jsx';
 
 const App = () => {
@@ -13,8 +12,6 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
-      {/* <Projects /> */}
-      {/* <Clients /> */}
       <WorkExperience />
       <Contact />
       <Footer />
