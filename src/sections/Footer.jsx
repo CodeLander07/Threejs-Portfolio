3f989d55b9b9e1f36a736f1c9634db3101b147ca
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-02-09 00:22:03 +0530

    Update Twitter SVG asset and adjust icon sizes in Footer component

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -12,13 +12,13 @@ const Footer = () => {
           <a href="https://github.com/unstopablesid" target="blank"><img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" /></a>
         </div>
         <div className="social-icon">
-          <a href="https://x.com/Mayur_nikumbh07" target="blank"><img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" /></a>
+          <a href="https://x.com/Mayur_nikumbh07" target="blank"><img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/3" /></a>
         </div>
         <div className="social-icon">
           <a href="https://www.instagram.com/mayurnikumbh07/" target="blank"><img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" /></a>
         </div>
         <div className="social-icon">
-          <a href="https://www.linkedin.com/in/mayur-nikumbh/" target="blank"><img src="/assets/linkedin.svg" alt="instagram" className="w-1/2 h-1/2" /></a>
+          <a href="https://www.linkedin.com/in/mayur-nikumbh/" target="blank"><img src="/assets/linkedin.svg" alt="instagram" className="w-1/2 " /></a>
         </div>
       </div>
 
